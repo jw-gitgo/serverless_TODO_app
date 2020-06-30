8e69e381c27acffd8e6fbf99e98e1580503c1abb
jw-gitgo <jmrwnd@yahoo.com>
2020-06-30 17:30:47 -0400

    getting Auth to work with Secrets Manager

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,11 +1,18 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
+import * as AWS from 'aws-sdk'
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+
+const client = new AWS.SecretsManager()
+let cachedSecret: string
 
 const logger = createLogger('auth')
 
@@ -57,7 +64,9 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
-  return undefined
+  const secretObject: any = await getSecret()
+  const secret = secretObject(secretField)
+  return verify(token, secret) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -72,3 +81,16 @@ function getToken(authHeader: string): string {
   return token
 }
 
+async function getSecret(){
+  if (cachedSecret) return cachedSecret
+
+  const data = await client
+    .getSecretValue({
+      SecretId: secretId
+    })
+    .promise()
+
+  cachedSecret = data.SecretString
+
+  return JSON.parse(cachedSecret)
+}
--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = 'os7rb1lhb4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-b0nbq31e.us.auth0.com',
+  clientId: '8CBxc51Man9BAROB7lPw00PlNTQgp7Ji',
   callbackUrl: 'http://localhost:3000/callback'
 }
