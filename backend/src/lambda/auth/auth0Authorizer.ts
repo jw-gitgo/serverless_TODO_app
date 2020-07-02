c20b04c16052a11cc73c76f269b830dae34d8df3
jw-gitgo <jmrwnd@yahoo.com>
2020-07-02 16:44:58 -0400

    trying different auth method

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,24 +1,14 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
-import 'source-map-support/register'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
+import 'source-map-support/register';
 
-//import * as AWS from 'aws-sdk'
-import { verify, decode } from 'jsonwebtoken'
-//import { verify } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
+import { verify, decode } from 'jsonwebtoken';
+import { createLogger } from '../../utils/logger';
+import Axios from 'axios';
+import { Jwt } from '../../auth/Jwt';
+import { JwtPayload } from '../../auth/JwtPayload';
 
-//const secretId = process.env.AUTH_0_SECRET_ID
-//const secretField = process.env.AUTH_0_SECRET_FIELD
-
-//const client = new AWS.SecretsManager()
-//let cachedSecret: string
-
-const logger = createLogger('auth')
-
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
-
+const logger = createLogger('auth');
+const jwksUrl = 'https://junglebadger.auth0.com/.well-known/jwks.json';
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,15 +51,10 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
-  const jwtKid = jwt.header.kid
-  let cert: string | Buffer
-
-  // TODO: Implement token verification
-  // const secretObject: any = await getSecret()
-  // const secret = secretObject(secretField)
-  // return verify(token, secret) as JwtPayload
+  const token = getToken(authHeader);
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt;
+  const jwtKid = jwt.header.kid;
+  let cert: string | Buffer;
 
   try {
     const jwks = await Axios.get(jwksUrl);
@@ -81,7 +66,6 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
     const { x5c } = signingKey;
 
     cert = `-----BEGIN CERTIFICATE-----\n${x5c[0]}\n-----END CERTIFICATE-----`;
-
   } catch (error) {
     console.log('Error While getting Certificate : ', error);
   }
@@ -100,19 +84,3 @@ function getToken(authHeader: string): string {
 
   return token
 }
-
-/*
-async function getSecret(){
-  if (cachedSecret) return cachedSecret
-
-  const data = await client
-    .getSecretValue({
-      SecretId: secretId
-    })
-    .promise()
-
-  cachedSecret = data.SecretString
-
-  return JSON.parse(cachedSecret)
-}
-*/
