c084aa14b6a0b8240eb25c0b5e3f0b7783f92279
jw-gitgo <jmrwnd@yahoo.com>
2020-07-04 10:45:05 -0400

    updated createTodo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,30 +1,48 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
+import * as uuid from 'uuid';
+import { decode } from 'jsonwebtoken';
+import { JwtPayload } from '../../auth/JwtPayload';
 import * as AWS from 'aws-sdk'
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-import * as uuid from 'uuid'
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
 
-// TODO: Implement creating a new TODO item
-// INITIAL ATTEMPT BELOW, based on 3.10
-
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('Processing event: ', event)
-  const itemId = uuid.v4()
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
 
-  const newItem = {
-    id: itemId,
-    newTodo
+  if (!newTodo.name) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'name is empty'
+      })
+    };
   }
+
+  const todoId = uuid.v4();
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const decodedJwt = decode(jwtToken) as JwtPayload
+  const userId = decodedJwt.sub;
+  const createdAt = new Date(Date.now()).toISOString();
+  
+
+  const todoItem = {
+    userId,
+    todoId,
+    createdAt,
+    done: false,
+    attachmentUrl: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${todoId}`
+  };
 
   await docClient.put({
     TableName: todosTable,
-    Item: newItem
-  }).promise()
-  
+    Item: todoItem
+  }).promise();
+
   return {
     statusCode: 201,
     headers: {
@@ -32,7 +50,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
-      item: newItem
+      item: todoItem
     })
-  }
+  };
 }
